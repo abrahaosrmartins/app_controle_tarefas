73209899b3921300aa79e99dd9b03f0e3f4f057b
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-14 02:50:19 -0300

    [Refactor] Changed password validation rule

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:4', 'confirmed'],
             // A validação 'confirmed', por trás dos panos, cria um segundo campo password_confirmation e o compara com o password original
             // De forma geral um campo <field>_confirmation é sempre comparado a um <field>
         ]);
