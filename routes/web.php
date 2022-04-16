15694b406c729d9eb0940abd4eaad4ab627d5c2d
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-16 03:33:24 -0300

    [Refactor] Removed home route

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::middleware(['verified'])->group(function (){
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::resource('tarefa', TarefaController::class);
 });
 
