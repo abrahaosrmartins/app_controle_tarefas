6dc745a02c5218a438595d3260e23c90857fc414
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-16 20:01:27 -0300

    [Feature] Added Export Class

---
+++ app/Exports/TarefasExport.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
+{
+    /**
+     * @return Collection
+     */
+    public function collection(): Collection
+    {
+        return auth()->user()->tarefas()->get();
+    }
+
+}
